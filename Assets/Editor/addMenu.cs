e7608546b559cd650124e860b9c06ebe2a142d95
clemldh <ldh5409@naver.com>
2017-10-15 15:12:08 +0900

    first

---
+++ Assets/Editor/addMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class TestBuilder : EditorWindow
+{
+
+    [MenuItem("CustomBuild/Build")]    
+
+    static void TestBuild()
+    {
+        AndroidBuild();       
+    }
+    
+
+    static void AndroidBuild()
+    {
+        BuildPipeline.BuildAssetBundles("AssetBundles", BuildAssetBundleOptions.None, BuildTarget.Android);
+
+        var scenes = EditorBuildSettings.scenes;
+
+        BuildPipeline.BuildPlayer(scenes, "Build/Test.apk", BuildTarget.Android, BuildOptions.None);
+
+
+    }
+
+}
---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour {
+
+    public string activeTag;
+
+    bool fallIn;
+
+    public bool IsFallin()
+    {
+        return fallIn;
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if(other.gameObject.tag == activeTag)
+        {
+            fallIn = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag==activeTag)
+        {
+            fallIn = false;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        Rigidbody r = other.gameObject.GetComponent<Rigidbody>();
+
+        Vector3 direction = transform.position - other.gameObject.transform.position;
+        direction.Normalize();
+
+        if(other.gameObject.tag == activeTag)
+        {
+            r.velocity *= 0.9f;
+
+            r.AddForce(direction * r.mass * 20.0f);
+        }
+        else
+        {
+            r.AddForce(-direction * r.mass * 80.0f);
+        }
+    }
+}
